df7d7751d2da6c823ef3d03fac79cc87031bdd67
Martijn de Vos <mdmartijndevos@gmail.com>
2020-10-21 20:36:20 +0200

    Added bandwidth statistics

--- chainsim/endpoint.py
+++ chainsim/endpoint.py
@@ -10,17 +10,20 @@ class PySimEndpoint(AutoMockEndpoint):
         self.env = env
         self.msg_queue = Store(env)
         self.env.process(self.process_messages())
+        self.bytes_up = 0
+        self.bytes_down = 0
 
     def process_messages(self):
         while True:
-            # Receive message
             from_address, packet = yield self.msg_queue.get()
+            self.bytes_down += len(packet)
             self.notify_listeners((from_address, packet))
 
     def send(self, socket_address, packet):
         if socket_address in internet:
             # For the unit tests we handle messages in separate asyncio tasks to prevent infinite recursion.
             ep = internet[socket_address]
+            self.bytes_up += len(packet)
             ep.msg_queue.put((self.wan_address, packet))
         else:
             raise AssertionError("Received data from unregistered address %s" % repr(socket_address))
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import random
 import shutil
 import sys
 import time
+from binascii import hexlify
 
 import yappi
 from simpy import Environment
@@ -20,13 +21,9 @@ NUM_PEERS = 100
 
 
 def setup_directories():
-    if not os.path.exists("data"):
-        os.mkdir("data")
-    if os.path.exists("data/detection_time.txt"):
-        os.remove("data/detection_time.txt")
-    if os.path.exists("data/fraud_time.txt"):
-        os.remove("data/fraud_time.txt")
+    shutil.rmtree("data", ignore_errors=True)
     shutil.rmtree("logs", ignore_errors=True)
+    os.mkdir("data")
     os.mkdir("logs")
 
 
@@ -69,6 +66,12 @@ def start_simulation(run_yappi=False):
         print("Simulated %d seconds... (peers that committed fraud: %d, exposed: %d)" % (second, global_vars.peers_committed_fraud, num_exposed))
         if num_exposed == NUM_PEERS:
             break
+
+    # Write bandwidth statistics
+    with open("data/bandwidth.csv", "w") as bw_file:
+        bw_file.write("public_key,bytes_up,bytes_down\n")
+        for node in nodes:
+            bw_file.write("%s,%d,%d\n" % (hexlify(node.my_peer.public_key.key_to_bin()).decode(), node.endpoint.bytes_up, node.endpoint.bytes_down))
 
     print("Simulation took %f seconds" % (time.time() - start_time))
 
