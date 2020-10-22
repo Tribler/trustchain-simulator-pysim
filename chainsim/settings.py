1a97d81d11a7c0a482f75cd1910ccd3ea0eb6809
Martijn de Vos <mdmartijndevos@gmail.com>
2020-10-22 18:04:19 +0200

    Made crawl interval dynamic

--- chainsim/settings.py
+++ chainsim/settings.py
@@ -7,4 +7,5 @@ class SimulationSettings:
     double_spend_probability = 0.1
     back_pointers = 10
     broadcast_fanout = 0
-    crawl_batch_size = 10
+    crawl_batch_size = 5
+    crawl_interval = 0.5
