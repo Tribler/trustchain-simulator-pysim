eb60a868da8ed8c08efb5ad436b123853635c3f0
Martijn de Vos <mdmartijndevos@gmail.com>
2020-10-29 18:12:39 +0100

    Fixed script

--- run_experiments_scalability.py
+++ run_experiments_scalability.py
@@ -30,9 +30,24 @@ if __name__ == "__main__":
 
         print("Evaluated batch 1")
 
-        # Batch 1
+        # Batch 2
         processes = []
-        for num_peers in [1000, 2000, 3000, 4000, 5000, 6000]:
+        for num_peers in [7000, 8000]:
+            settings = SimulationSettings()
+            settings.peers = num_peers
+            settings.exchange_strategy = strategy
+            p = Process(target=run, args=(settings,))
+            p.start()
+            processes.append(p)
+
+        for p in processes:
+            p.join()
+
+        print("Evaluated batch 2")
+
+        # Batch 3
+        processes = []
+        for num_peers in [9000, 10000]:
             settings = SimulationSettings()
             settings.peers = num_peers
             settings.exchange_strategy = strategy
