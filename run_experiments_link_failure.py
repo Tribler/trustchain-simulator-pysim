c3060ecabf9c8c012f4c761a1b8b1967147a0678
Martijn de Vos <mdmartijndevos@gmail.com>
2020-10-29 11:32:57 +0100

    Added link failure experiment script

---
+++ run_experiments_link_failure.py
@@ -0,0 +1,31 @@
+from multiprocessing.context import Process
+
+from simpy import Environment
+
+from chainsim.settings import SimulationSettings
+from chainsim.simulation import TrustChainSimulation
+
+
+def run(settings):
+    env = Environment()
+    simulation = TrustChainSimulation(settings, env)
+    simulation.run()
+
+
+if __name__ == "__main__":
+
+    for strategy in [0, 1, 2, 3]:
+        processes = []
+        for link_failure in range(0, 20, 2):
+            settings = SimulationSettings()
+            settings.peers = 1000
+            settings.send_fail_probability = link_failure / 100
+            settings.exchange_strategy = strategy
+            p = Process(target=run, args=(settings,))
+            p.start()
+            processes.append(p)
+
+        for p in processes:
+            p.join()
+
+        print("Fully evaluated strategy %d!" % strategy)
