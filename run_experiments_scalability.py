a341ad2126d5b5955b88a94c99d75c40f2a9a9e6
Martijn de Vos <mdmartijndevos@gmail.com>
2020-10-29 17:49:33 +0100

    Added scalability experiment script

---
+++ run_experiments_scalability.py
@@ -0,0 +1,46 @@
+from multiprocessing.context import Process
+
+from simpy import Environment
+
+from chainsim.settings import SimulationSettings
+from chainsim.simulation import TrustChainSimulation
+
+
+def run(settings):
+    env = Environment()
+    simulation = TrustChainSimulation(settings, env)
+    simulation.run()
+
+
+if __name__ == "__main__":
+
+    for strategy in [0, 1, 2, 3]:
+        # Batch 1
+        processes = []
+        for num_peers in [1000, 2000, 3000, 4000, 5000, 6000]:
+            settings = SimulationSettings()
+            settings.peers = num_peers
+            settings.exchange_strategy = strategy
+            p = Process(target=run, args=(settings,))
+            p.start()
+            processes.append(p)
+
+        for p in processes:
+            p.join()
+
+        print("Evaluated batch 1")
+
+        # Batch 1
+        processes = []
+        for num_peers in [1000, 2000, 3000, 4000, 5000, 6000]:
+            settings = SimulationSettings()
+            settings.peers = num_peers
+            settings.exchange_strategy = strategy
+            p = Process(target=run, args=(settings,))
+            p.start()
+            processes.append(p)
+
+        for p in processes:
+            p.join()
+
+        print("Fully evaluated strategy %d!" % strategy)
