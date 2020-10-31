f5cf68c94cb30118750c82fd5eb5d3bb43939cc3
Martijn de Vos <mdmartijndevos@gmail.com>
2020-10-31 13:14:14 +0100

    Changed scalability experiment script

--- run_experiments_scalability.py
+++ run_experiments_scalability.py
@@ -1,3 +1,4 @@
+import sys
 from multiprocessing.context import Process
 
 from simpy import Environment
@@ -13,49 +14,22 @@ def run(settings):
 
 
 if __name__ == "__main__":
+    strategy = int(sys.argv[1])
 
-    for strategy in [0, 1, 2, 3]:
-        # Batch 1
-        processes = []
-        for num_peers in [1000, 2000, 3000, 4000, 5000, 6000]:
-            settings = SimulationSettings()
-            settings.peers = num_peers
-            settings.exchange_strategy = strategy
-            p = Process(target=run, args=(settings,))
-            p.start()
-            processes.append(p)
+    print("Going to evaluate strategy %d" % strategy)
 
-        for p in processes:
-            p.join()
+    # Batch 1
+    processes = []
+    for num_peers in range(1000, 11000, 1000):
+        print("Evaluating %d peers..." % num_peers)
+        settings = SimulationSettings()
+        settings.peers = num_peers
+        settings.exchange_strategy = strategy
+        p = Process(target=run, args=(settings,))
+        p.start()
+        processes.append(p)
 
-        print("Evaluated batch 1")
+    for p in processes:
+        p.join()
 
-        # Batch 2
-        processes = []
-        for num_peers in [7000, 8000]:
-            settings = SimulationSettings()
-            settings.peers = num_peers
-            settings.exchange_strategy = strategy
-            p = Process(target=run, args=(settings,))
-            p.start()
-            processes.append(p)
-
-        for p in processes:
-            p.join()
-
-        print("Evaluated batch 2")
-
-        # Batch 3
-        processes = []
-        for num_peers in [9000, 10000]:
-            settings = SimulationSettings()
-            settings.peers = num_peers
-            settings.exchange_strategy = strategy
-            p = Process(target=run, args=(settings,))
-            p.start()
-            processes.append(p)
-
-        for p in processes:
-            p.join()
-
-        print("Fully evaluated strategy %d!" % strategy)
+    print("Fully evaluated strategy %d!" % strategy)
